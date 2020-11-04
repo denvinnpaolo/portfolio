184cca0cfe416a3bb471ff387b8577fbe5b9f11d
Denvinn Magsino <denvinnpaolo@gmail.com>
2020-11-03 23:01:49 -0800

    "about me" txt from black to white

--- frontend/src/landing-page/components/about.js
+++ frontend/src/landing-page/components/about.js
@@ -8,7 +8,7 @@ const About = props => {
         <div>
             <div><img src={profile} height='200px' /></div>
             <div>Software Engineer/Full Stack Developer</div>
-            <div>My name is Denvinn Paolo Magsino and I have 2 years experience in software engineering and 
+            <div id="txts">My name is Denvinn Paolo Magsino and I have 2 years experience in software engineering and 
                 web development. I have acquired my certification at Lambda School. My experience being a student
                 had made me grow in way I cannot imagine. I can build an application from top to bottom. 
             </div>
