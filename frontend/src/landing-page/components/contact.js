9d4daedfdfa79fba40ce9ab94355bd7ddaffdae7
Denvinn Magsino <denvinnpaolo@gmail.com>
2020-11-04 22:58:51 -0800

    added linkdIn

--- frontend/src/landing-page/components/contact.js
+++ frontend/src/landing-page/components/contact.js
@@ -4,7 +4,8 @@ const Contact = props => {
     const [contact, setContact] = useState({
         number: '818-404-5749',
         email: 'denvinnpaolo@gmail.com',
-        address: 'Los Angeles, CA'
+        address: 'Los Angeles, CA',
+        linkdIn: 'https://www.linkedin.com/in/denvinn-magsino/'
     })
 
     return(
